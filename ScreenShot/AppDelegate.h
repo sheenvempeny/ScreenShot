04915183621a8146bf84b0b8cee2d69a92d981ea
sheenvempeny <sheen_vempeny@hotmail.com>
2016-01-27 18:02:26 +0400

    Project Created

---
+++ ScreenShot/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ScreenShot
+//
+//  Created by Sheen on 1/27/16.
+//  Copyright © 2016 Sheen. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ ScreenShot/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  ScreenShot
+//
+//  Created by Sheen on 1/27/16.
+//  Copyright © 2016 Sheen. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
